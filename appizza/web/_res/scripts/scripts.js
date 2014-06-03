fc32a9c7777d3bcd83bca9148e18a7afa8a7658a
danielvictorferrari@gmail.com <danielvictorferrari@gmail.com@8ac308e6-b1aa-ba51-10a8-91d4d0fc66a1>
2014-06-03 11:07:16 +0000

    Biblioteca jquery, js para organizar tabelas, script para páginas em geral

---
+++ appizza/web/_res/scripts/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    exibirDiv("#container", "opt-1");
+   
+    //transição de opções do menu
+    $("#top-menu ul li a").click(function(){
+        $("#top-menu ul li").removeClass('selected');
+        $(this).parent().addClass('selected');
+        exibirDiv("#container", $(this).attr("id"));
+    });
+
+    //transição de divs com conteúdo para cada página
+    function exibirDiv(elementContainer, opt) {
+        var elementId = "";
+        
+        $(elementContainer).children().css("display", "none");
+
+        if (opt === "opt-1") 
+            elementId = "#search-container";
+        else if (opt === "opt-2")
+            elementId = "#add-container";
+        else if (opt === "opt-3")
+            elementId = "#report-container";
+
+        $(elementId).css("display", "");
+    }
+});
+
+    function subMenu() {
+        $(this).get(".sub").css("display", "");
+    }
